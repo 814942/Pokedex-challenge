e53a0b51871ca6962e4060b8e6b8e8b92b8998c0
Pablo Garay <elxebec@gmail.com>
2024-02-15 19:28:23 -0300

    Change server port

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import 'dotenv/config'
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req: Request, res: Response)=>{
   res.send('Hello, Natural Tech House');
