712dd0f285c94df8453036dd0eb5985183b7de2c
Pablo Garay <elxebec@gmail.com>
2024-02-16 20:38:22 -0300

    Implement swagger

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,11 +1,12 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from './swagger';
 import cors from "cors";
 import 'dotenv/config'
 
-import verifyToken from './auth/verifyToken';
-import routes from "./routes";
+import verifyToken from './middlewares/verifyToken';
+import router from "./router";
 import login from './controllers/login';
-
 const { PORT } = process.env
 
 const server: Express = express();
@@ -17,21 +18,23 @@ const corsOptions = {
   "credentials": true
 }
 
+// Middlewares
 server.use(cors(corsOptions));
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
-// Routes
+// router
 server.post("/login", login);
-
-server.use('/protected', verifyToken, routes);
+server.use('/protected', verifyToken, router);
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Error catching endware.
-server.use((err: Error, req: Request, res: Response) => {
-  const message = err.message || err;
-  console.error("Server error: ", err);
-  res.status(500).send(message);
-});
+// TODO: implementar error handling
+// server.use((err: Error, req: Request, res: Response) => {
+//   const message = err.message || err;
+//   console.error("Server error: ", err);
+//   res.status(500).send(message);
+// });
 
 server.listen(port, () => {
   console.log(`[Server]: I am running at https://localhost:${port}`);
